cd7bbd1c126fae40a118d6fc837d60ebd55b8a9e
vilhota30 <nirodalina@gmail.com>
2023-04-30 21:06:00 +0300

    завдання 2
    завантажує бібліотеки "@vimeo/player" та "lodash.throttle":

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import "../css/common.css";
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const TIME_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+
+const player = new player(iframe);
+
+const onPlay = function (data) {
+    const strData = JSON.stringify(data);
+    localStorage.setItem(TIME_KEY, strData);
+    player.on("timeupdate", throttle(onPlay, 1000));
+}
+
+function playBackResult() {
+    if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+        return;
+    }
+
+    const paused = JSON.parce(localStorage.getItem(TIME_KEY))["seconds"];
+    if (paused) {
+        player 
+            .setCurrentTime(paused)
+            .then(function (seconds) {})
+            .catch(function (error) {
+                switch (error.name) {
+                    case "Error":
+                        breack;
+                    default:
+                        "new problem, plaese wait a few minutes"
+                        breack;
+            }
+        })
+    }
+}
+
 
