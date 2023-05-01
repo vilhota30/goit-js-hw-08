3352beb90d9ff31f30059672d4935fb4f13283de
vilhota30 <nirodalina@gmail.com>
2023-05-01 18:19:55 +0300

    виконує  завдання 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    feedback: document.querySelector('[name="message"]'),
+};
+autocompleteInputFromLocalStorage();
+const feedbackData = {};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFeedbackFormInput, 1000));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.removeItem('feedback-form-state');
+
+}
+
+function onFeedbackFormInput(event) {
+    feedbackData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackData));
+
+}
+
+
+function autocompleteInputFromLocalStorage() {
+    const dataInputs = localStorage.getItem('feedback-form-state');
+    if (dataInputs) {
+        const inputArray = JSON.parse(localStorage.getItem('feedback-form-state'));
+        refs.email.value = inputArray.email;
+        refs.feedback.value = inputArray.message;
+
+        console.log(inputArray);
+
+        console.log(refs.email.value);
+        console.log(refs.feedback.value);
+        
+    }
+ }
+
 
